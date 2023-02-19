d31ec01786297861d99e8152b5a16f7e324e9faf
duck-ghost <113725669+duck-ghost@users.noreply.github.com>
2023-02-19 17:37:30 +0000

    feat(percentage): add Turkish phrase

--- src/interactions/fun/percentage.ts
+++ src/interactions/fun/percentage.ts
@@ -51,7 +51,11 @@ export class percentage extends QInteraction {
                         {
                             name: "Attractiveness",
                             value: "Attractive"
-                        }
+                        },
+                        {
+                            name: "Turkish",
+                            value: "Turkish 🇹🇷"
+                        },
                     )
                 )
         );
@@ -80,6 +84,10 @@ export class percentage extends QInteraction {
             )
             .setThumbnail(user.avatarURL())
             .setColor([red, green, blue]);
+
+        if (phrase === "Turkish 🇹🇷" && percentage >= 50) {
+            embed.setImage('https://media.discordapp.net/attachments/934591528083529738/1060649095741591705/IMG_2292.png');
+        }
 
         // Sending reply
         interaction.reply({
