71c208a255b022677e3f3524d02f5ecf4fb90241
quantix-dev <9021453+quantix-dev@users.noreply.github.com>
2023-02-23 22:35:11 +0000

    feat(starboard): add basic embed for starred messages

--- src/events/PostMessageReact.ts
+++ src/events/PostMessageReact.ts
@@ -1,4 +1,4 @@
-import { MessageReaction, User } from "discord.js";
+import { EmbedBuilder, MessageReaction, User } from "discord.js";
 import { PostEvent } from "../utils/events/PostEvent";
 import { QEvent } from "../utils/events/BaseEvent";
 
@@ -8,6 +8,22 @@ export class PostMessageReact extends PostEvent {
     }
 
     public execute(client: QEvent.QClient, reaction: MessageReaction, reactor: User) {
-        console.log("TODO: Handle Emoji");
+        const reactionCount = reaction.count;
+        const message = reaction.message;
+
+        // Delete self starrers
+        if (reactor === message.author) {
+            return message.delete();
+        }
+
+        if (reactionCount >= 2 && message.author) {
+            const starboardEmbed = new EmbedBuilder()
+                .setAuthor({
+                    name: message.author.username,
+                    iconURL: message.author.avatarURL() || ""
+                })
+                .setDescription(message.content);
+            message.channel.send({ embeds: [starboardEmbed] });
+        }
     }
 }
