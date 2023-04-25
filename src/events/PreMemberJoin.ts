504d1e85092324b4385925ce6f163e9dc76a363c
Quantix <9021453+quantix-dev@users.noreply.github.com>
2023-04-25 23:38:57 +0100

    feat(verify): automatically verify members on join (#17)
    * feat(verify): add guildMemberAdd event for verification
    
    * fix(verify): select first GuildMember argument
    
    * chore: add GuildMembers intent

---
+++ src/events/PreMemberJoin.ts
@@ -0,0 +1,23 @@
+import { GuildMember } from "discord.js";
+import { QEvent } from "../utils/events/BaseEvent";
+import { PreEvent } from "../utils/events/PreEvent";
+import { updateAccount } from "../interactions/verification/verify";
+import { getGuild } from "../models/Guild";
+
+export class PreMemberJoin extends PreEvent {
+    public constructor() {
+        super("guildMemberAdd");
+    }
+
+    public async execute(client: QEvent.QClient, members: GuildMember[]) {
+        const member = members[0];
+
+        // Getting joined guild record
+        const guild = await getGuild(member.guild.id);
+        if (!guild) return false;
+
+        // Attempting to update account
+        const didUpdate = await updateAccount(guild, member);
+        return didUpdate;
+    }
+}
